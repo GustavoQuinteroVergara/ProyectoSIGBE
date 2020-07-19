33a2a21fc2375fd336818d31a37bc580e599d8ce
GustavoQuinteroVergara <quintero.gustavo@correounivalle.edu.co>
2020-07-19 15:31:55 -0500

    Modificaciones a componente convocatoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrarConvocatoriaComponent } from './components/convocatoria/registrar-convocatoria/registrar-convocatoria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarConvocatoriaComponent
   ],
   imports: [
     BrowserModule
