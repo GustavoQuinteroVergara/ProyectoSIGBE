ede8e3b02a082781ca3dcac643ff8e733f459426
AlejoOspina01 <alejandro.ospina@correounivalle.edu.co>
2020-07-20 15:58:08 -0500

    Se agrega el servicio de login y se pone funcional

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import {ServiceloginService} from './servicelogin.service';
+import {Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  usuario= null;
 
-  ngOnInit(): void {
+  constructor(private logins:ServiceloginService,private router:Router) { }
+
+  buscarUsuario(email:string, contrasena:string){
+    this.logins.buscarUser(email).subscribe(result =>{ 
+      console.log(result[0].contrasena); 
+      console.log(contrasena);
+      if(result[0].contrasena == contrasena){
+        console.log("inicio exitoso");
+        this.router.navigate(['/registroConvocatoria']);
+      }else{
+        console.log("error");
+      }
+      this.usuario = result; });
+    console.log(this.usuario);
+    
   }
-
 }
