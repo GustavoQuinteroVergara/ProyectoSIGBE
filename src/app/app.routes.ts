60837a8f37d2e52e69e93a6362f4abdafd803e53
GustavoQuinteroVergara <quintero.gustavo@correounivalle.edu.co>
2020-07-20 11:44:35 -0500

    Creación frontend convocatoria y modificación index, agregados scripts materialize

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import {Routes } from '@angular/router';
 /*Estos nombres se traen de como aparece la clase en el .ts(Este caso registrar)*/
-/*import {RegistrarConvocatoriaComponent} from './components/convocatoria/registrar-convocatoria/registrar-convocatoria.component'*/
+import {RegistrarConvocatoriaComponent} from './components/convocatoria/registrar-convocatoria/registrar-convocatoria.component';
 
 export const ROUTES: Routes = [
     /*en PATH : Se pone un nombre cualquiera para luego utilizarlo en el menu - 
     Component : el mismo nombre del import */
-    /*{path:'registroConvocatoria', component: RegistrarConvocatoriaComponent}*/
+    {path:'registroConvocatoria', component: RegistrarConvocatoriaComponent}
 
     /*
     {path: '', pathMatch:'full', redirectTo:'home'},
