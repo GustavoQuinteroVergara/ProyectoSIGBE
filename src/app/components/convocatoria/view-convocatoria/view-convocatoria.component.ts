244e49232d4f0d3d5e06d29cbdf166679b3790ff
GustavoQuinteroVergara <quintero.gustavo@correounivalle.edu.co>
2020-08-20 22:14:32 -0500

    Se realiza cambio al diseño y funcionalidad de actualizar convocatoria

--- src/app/components/convocatoria/view-convocatoria/view-convocatoria.component.ts
+++ src/app/components/convocatoria/view-convocatoria/view-convocatoria.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import {ServicesViewConvocatoriaService} from './services-view-convocatoria.service';
 import {ServiciolistarconvoService} from './../../listarconvocatoria/serviciolistarconvo.service';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
+import {RegistrarConvoServiceService} from '../registrar-convocatoria/registrar-convo-service.service';
 
 @Component({
   selector: 'app-view-convocatoria',
@@ -12,16 +12,22 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 })
 export class ViewConvocatoriaComponent {
 
-	$idConvo:any;
-	$postuByIdArray:any;
-	$convoBuscada:any;
+  $idConvo:any;
+  $postuByIdArray:any;
+  $convoBuscada:any;
   loading=false;
-
+  convoActualizado:any;
+  success:any;
+  convocatoriaBeca: any;
+  convocatoriaPeriodo: any;
   constructor(private rutaActiva: ActivatedRoute,
-   private serviceviewconvocatoria:ServicesViewConvocatoriaService,public dialog: MatDialog) { 
-  	this.$idConvo = this.rutaActiva.snapshot.params.idConvo;
-  	this.buscarConvoByIdConvo(this.$idConvo);
-  	this.buscarPostulacionesByIdConvo(this.$idConvo);
+   private serviceviewconvocatoria:ServicesViewConvocatoriaService,public dialog: MatDialog, 
+   private serviceConvocatoria:RegistrarConvoServiceService ) { 
+    this.$idConvo = this.rutaActiva.snapshot.params.idConvo;
+    this.buscarConvoByIdConvo(this.$idConvo);
+    this.buscarPostulacionesByIdConvo(this.$idConvo);
+    this.buscarBeca();
+    this.buscarPeriodo();
   }
 
   buscarEstPostuByIden(templateRef){
@@ -31,21 +37,53 @@ export class ViewConvocatoriaComponent {
        });
   }
 
-	buscarConvoByIdConvo(idConvo:any){
-  	  this.serviceviewconvocatoria.buscarConvoById(idConvo).subscribe(result =>{ 
+  buscarConvoByIdConvo(idConvo:any){
+      this.serviceviewconvocatoria.buscarConvoById(idConvo).subscribe(result =>{ 
       console.log(result); 
       this.$convoBuscada = result; 
       this.loading=true;
- 	});
+  });
   }
 
   buscarPostulacionesByIdConvo(idConvo:any){
-  	  this.serviceviewconvocatoria.buscarPostulacionesByIdConvo(idConvo).subscribe(result =>{ 
+      this.serviceviewconvocatoria.buscarPostulacionesByIdConvo(idConvo).subscribe(result =>{ 
       console.log(result); 
       this.$postuByIdArray = result; 
- 	});
+  });
   }
-
-
-
+  actualizarConvocatoria(consecutivo_convocatoria:any,cupo:any,becas:any,periodosacademicos:any,
+    fecha_inicio:any,fecha_fin:any,estado_convocatoria:any){
+    this.convoActualizado= {
+      consecutivo_convocatoria:consecutivo_convocatoria,
+      cupo:cupo,
+      becas:becas,
+      periodosacademicos:periodosacademicos,
+      fecha_inicio:fecha_inicio,
+      fecha_fin:fecha_fin,
+      estado_convocatoria:estado_convocatoria,
+    };
+  this.serviceviewconvocatoria.actualizarListConvocatorias(this.convoActualizado).subscribe
+    (res=>{
+      
+    },(err)=>{
+      //console.log('ERROR: ' + err.error.text);
+      
+    });
+    console.log(this.convoActualizado);
+  
+  }
+  buscarBeca(){
+    this.serviceConvocatoria.buscarListadoBecas().subscribe(convocatoriaBeca=>{
+      console.log(convocatoriaBeca);
+      this.convocatoriaBeca = convocatoriaBeca;
+    });
+  }
+  buscarPeriodo(){
+    this.serviceConvocatoria.buscarListadoPeriodos().subscribe(convocatoriaPeriodo=>{
+      console.log(convocatoriaPeriodo);
+      this.convocatoriaPeriodo = convocatoriaPeriodo;
+    });
+  }
+  
 }
+
