917f630fc863b98cde178dacf2386991c8fd1b68
AlejoOspina01 <alejandro.ospina@correounivalle.edu.co>
2020-08-31 15:45:29 -0500

    Se arregla en comprar ticket, actualizar el saldo.

--- src/app/components/crear-ticket/crear-ticket.component.ts
+++ src/app/components/crear-ticket/crear-ticket.component.ts
@@ -4,6 +4,7 @@ import {Globals} from './../../global';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 import {TicketService} from '../../services/ticket.service';
+import {UsuarioService} from '../../services/usuario.service';
 
 @Component({
   selector: 'app-crear-ticket',
@@ -16,11 +17,17 @@ export class CrearTicketComponent {
   fechaActual= new Date();
   todaysDataTime = '';
   ticketArray:any;
+
+  userArraySaldo:any;
   valorTicket:number;
   success=false;
+  confSaldo = true;
+  confErr = false;
+  confHorario = true;
   $nombreusuario= JSON.parse(localStorage.getItem('currentUser'));
 
-  constructor(globals: Globals,private ticketService:TicketService,public dialog: MatDialog) { 
+  constructor(globals: Globals,private ticketService:TicketService,
+    private usuarioService:UsuarioService,public dialog: MatDialog) { 
   	this.globals = globals;
 
   	// Horas manejada en 24:00HS
@@ -37,36 +44,113 @@ export class CrearTicketComponent {
   }
 
   comprarTicket(templateRef){
+      this.success=false;
+      this.confSaldo = true;
+      this.confErr = false;
+      this.confHorario = true;
   	this.ticketArray= {
   		idUser: this.$nombreusuario.identi,
   		valorticket:this.valorTicket,
   		tipoTicket:this.tipoBeca
   	}
+    this.userArraySaldo = {
+      idUser : this.$nombreusuario.identi,
+      saldo : this.$nombreusuario.saldo - this.valorTicket
+    }
 
   	if((this.todaysDataTime > this.globals.horainicioVentaAlmuerzo) &&  (this.todaysDataTime < this.globals.horaFinVentaAlmuerzo)){
-  		this.success=true;
-  		let dialogRef = this.dialog.open( templateRef,{
+      //inicio primer if
+
+      if(this.userArraySaldo.saldo >= 0){
+
+        	this.ticketService.registrarTicket(this.ticketArray).subscribe(result=>{
+        		
+            
+            this.usuarioService.actualizarSaldo(this.userArraySaldo).subscribe(result=>{
+              this.$nombreusuario.saldo = this.$nombreusuario.saldo - this.valorTicket;
+              this.success=true;
+              localStorage.setItem('currentUser',JSON.stringify(this.$nombreusuario));
+              let dialogRef = this.dialog.open( templateRef,{
+                height: '200px',
+                width: '200px',
+              });
+              console.log("Actualizado, msg " ,result);
+
+            },(err)=>{
+              this.confErr = true;
+              let dialogRef = this.dialog.open( templateRef,{
+                height: '200px',
+                width: '200px',
+              });
+              console.log(err.error);
+            });
+        	},(err)=>{
+            this.confErr = true;
+              let dialogRef = this.dialog.open( templateRef,{
+                height: '200px',
+                width: '200px',
+              });
+              console.log(err.error);
+        	});
+
+      }else{
+        this.confSaldo = false;
+      let dialogRef = this.dialog.open( templateRef,{
         height: '200px',
         width: '200px',
       });
-  	this.ticketService.registrarTicket(this.ticketArray).subscribe(result=>{
-  		this.success=false;
-  	},(err)=>{
-  		this.success=false;
-  	});
+
+      }
+
+
+    //fin primer if
   	}else if((this.todaysDataTime > this.globals.horainicioVentaRefrigerio) &&  
   		(this.todaysDataTime < this.globals.horaFinVentaRefrigerio)){
-  	this.success=true;
-  	let dialogRef = this.dialog.open( templateRef,{
+      // Inicio segundo if
+        if(this.userArraySaldo.saldo >= 0){
+
+          this.ticketService.registrarTicket(this.ticketArray).subscribe(result=>{
+            
+            
+            this.usuarioService.actualizarSaldo(this.userArraySaldo).subscribe(resultus=>{
+              this.$nombreusuario.saldo = this.$nombreusuario.saldo - this.valorTicket;
+              this.success=true;
+              localStorage.setItem('currentUser',JSON.stringify(this.$nombreusuario));
+              let dialogRef = this.dialog.open( templateRef,{
+                height: '200px',
+                width: '200px',
+              });
+              
+              console.log("Actualizado, msg " ,resultus);
+
+            },(errs)=>{
+              this.confErr = true;
+              let dialogRef = this.dialog.open( templateRef,{
+                height: '200px',
+                width: '200px',
+              });
+              console.log(errs.error);
+            });
+          },(err)=>{
+              this.confErr = true;
+              let dialogRef = this.dialog.open( templateRef,{
+                height: '200px',
+                width: '200px',
+              });
+          });
+
+      }else{
+        this.confSaldo = false;
+      let dialogRef = this.dialog.open( templateRef,{
         height: '200px',
         width: '200px',
       });
-  	this.ticketService.registrarTicket(this.ticketArray).subscribe(result=>{
-  		this.success=false;
-  	},(err)=>{
-  		this.success=false;
-  	});
+
+      }
+
+    // Fin segundo if
   	}else{
+      this.confHorario = false;
   		let dialogRef = this.dialog.open( templateRef,{
         height: '200px',
         width: '200px',
