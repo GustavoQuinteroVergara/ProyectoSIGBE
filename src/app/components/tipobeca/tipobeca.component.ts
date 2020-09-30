d94cfed57918d3d6c2ca3ec4e1e2dbaa312d6deb
GustavoQuinteroVergara <quintero.gustavo@correounivalle.edu.co>
2020-09-30 13:39:20 -0500

    Se realiza responsive, manejo de alertas al momento  de registrar

--- src/app/components/tipobeca/tipobeca.component.ts
+++ src/app/components/tipobeca/tipobeca.component.ts
@@ -1,23 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import {TipobecaService} from './tipobeca.service';
 @Component({
   selector: 'app-tipobeca',
   templateUrl: './tipobeca.component.html',
   styleUrls: ['./tipobeca.component.css']
 })
-export class TipobecaComponent  {
+export class TipobecaComponent implements OnInit {
   registroTBeca:any[]=[];
   tipoBecaRegistrar:any;
-  constructor(private serviceTipoBeca : TipobecaService) { }
+  success:any;
+  public formularioTipoBeca: FormGroup;
+  constructor(private serviceTipoBeca : TipobecaService,public dialog: MatDialog) { }
 
-  registrarTBeca(descripcion:any){
+  ngOnInit(): void {
+    this.formularioTipoBeca = new FormGroup({
+      descripcionTipoBeca: new FormControl('',Validators.required),
+    });
+  }
+  public hasError = (controlName: string, errorName: string) =>{
+    return this.formularioTipoBeca.controls[controlName].hasError(errorName);
+  }
+  registrarTBeca(descripcion:any,templateRef){
     this.tipoBecaRegistrar= {descripcion:descripcion};
     this.serviceTipoBeca.registrarTipoBeca(this.tipoBecaRegistrar).subscribe(res =>{
+      this.success = true;
+      let dialogRef = this.dialog.open( templateRef,{
+         height: '200px',
+         width: '200px',
+       });
       console.log(res);
   
   
       },(err)=>{
-      
+        this.success = false;
+        let dialogRef = this.dialog.open( templateRef,{
+           height: '200px',
+           width: '200px',
+         });
   
          console.log('ERROR: ' + err.error.text);
       });
