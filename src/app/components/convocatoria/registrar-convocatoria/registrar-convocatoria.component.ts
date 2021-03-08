117122d595b99fbe7bd06e4a39ca073dadb60174
GustavoQuinteroVergara <quintero.gustavo@correounivalle.edu.co>
2021-03-08 17:52:14 -0500

    Se agregan checkbox con su respectiva funcionalidad

--- src/app/components/convocatoria/registrar-convocatoria/registrar-convocatoria.component.ts
+++ src/app/components/convocatoria/registrar-convocatoria/registrar-convocatoria.component.ts
@@ -14,6 +14,28 @@ export class RegistrarConvocatoriaComponent implements OnInit {
   convocatoriaPeriodo: any;
   convocatoriaRegistrar:any;
   success:any;
+  isChecked=false;
+  //Documentos beca alimenticia
+  documentoest=false;
+  documentoacu=false;
+  pagomatricula=false;
+  facturaserv=false;
+  tab=false;
+  certingresos=false;
+  formsolicitud=false;
+  carta=false;
+  vecindad=false;
+  //documentos apoyo socioeconomico
+  constancia=false;
+  promacumulado=false;
+  //documentos monitoria
+  diagnosticomed=false;
+  formatod10=false;
+  //bono estudiantil
+  documentopadre=false;
+  documentomadre=false;
+  carnet=false;
+
   public formularioConvocatoria: FormGroup;
   
   public disabled = false;
@@ -22,6 +44,7 @@ export class RegistrarConvocatoriaComponent implements OnInit {
   public touchUi = false;
   public enableMeridian = false;
   opcion=false;
+  fotocopia=false;
   asuntocorreo:any;
   contenidocorreo:any;
   
@@ -78,8 +101,12 @@ export class RegistrarConvocatoriaComponent implements OnInit {
     });
   }
 
-  registrarConvocatoria(fechainicial:any,fechafinal:any,becas:any,cupos:any,estadoconvocatoria:any,asuntocorreo:any,contenidocorreo:any,templateRef){
-
+  registrarConvocatoria(fechainicial:any,fechafinal:any,becas:any,cupos:any,estadoconvocatoria:any,
+    asuntocorreo:any,contenidocorreo:any,
+    d10:any,factserv:any,cartapeti:any,carnetest:any,cedulapadre:any,cedulamadre:any,
+    promedio:any,tabulado:any,constanciaweb:any,certificadovencidad:any,documentoidenti:boolean,
+    documentoacudiente:any,formatosolicitud:any,diagnostico:any,recibomatricula:any,
+    certificadoingresos:any,templateRef){
 
     if(this.opcion){
       this.convocatoriaRegistrar= {fechainicio:fechainicial,
@@ -88,9 +115,25 @@ export class RegistrarConvocatoriaComponent implements OnInit {
         cupo:cupos,
         periodo:this.convocatoriaPeriodo[0].consecutivo_periodo,
         estadoconvocatoria:estadoconvocatoria,
-        enviarCorreo:true,
+        enviarCorreo:false,
         asuntocorreo:asuntocorreo,
-        contenidocorreo:contenidocorreo
+        contenidocorreo:contenidocorreo,
+        d10:this.formatod10,
+        factserv:this.facturaserv,
+        cartapeti:this.carta,
+        carnetest:this.carnet,
+        cedulapadre:this.documentopadre,
+        cedulamadre:this.documentomadre,
+        promedio:this.promacumulado,
+        tabulado:this.tab,
+        constanciaweb:this.constancia,
+        certificadovencidad:this.vecindad,
+        documentoidenti:this.documentoest,
+        documentoacudiente:this.documentoacu,
+        formatosolicitud:this.formsolicitud,
+        diagnostico:this.diagnosticomed,
+        recibomatricula:this.pagomatricula,
+        certificadoingresos:this.certingresos        
       };
     }else{
       this.convocatoriaRegistrar= {fechainicio:fechainicial,
@@ -101,7 +144,23 @@ export class RegistrarConvocatoriaComponent implements OnInit {
         estadoconvocatoria:estadoconvocatoria,
         enviarCorreo:false,
         asuntocorreo:'',
-        contenidocorreo:''
+        contenidocorreo:'',
+        d10:this.formatod10,
+        factserv:this.facturaserv,
+        cartapeti:this.carta,
+        carnetest:this.carnet,
+        cedulapadre:this.documentopadre,
+        cedulamadre:this.documentomadre,
+        promedio:this.promacumulado,
+        tabulado:this.tab,
+        constanciaweb:this.constancia,
+        certificadovencidad:this.vecindad,
+        documentoidenti:this.documentoest,
+        documentoacudiente:this.documentoacu,
+        formatosolicitud:this.formsolicitud,
+        diagnostico:this.diagnosticomed,
+        recibomatricula:this.pagomatricula,
+        certificadoingresos:this.certingresos        
       };
     }
     this.serviceConvocatoria.registrarConvocatoria(this.convocatoriaRegistrar).subscribe(res =>{
@@ -130,7 +189,127 @@ export class RegistrarConvocatoriaComponent implements OnInit {
     }else{
       this.opcion = true;
     }
+    
 
   }
+  
+  setAllDocumentoEstudiante(completed:boolean){
+    if(this.documentoest){
+      this.documentoest=false;
+    }else{
+     this.documentoest=true;     
+    }
+  }
 
+  documentoAcudiente(completed:boolean){
+    if(this.documentoacu){
+      this.documentoacu=false;
+    }else{
+      this.documentoacu=true;     
+    }
+  }
+  pagoMat(completed:boolean){
+    if(this.pagomatricula){
+      this.pagomatricula=false;
+    }else{
+      this.pagomatricula=true;     
+    }
+  }
+  reciboPublico(completed:boolean){
+    if(this.facturaserv){
+      this.facturaserv=false;
+    }else{
+      this.facturaserv=true;     
+    }
+  }
+  documentoTabulado(completed:boolean){
+    if(this.tab){
+      this.tab=false;
+    }else{
+      this.tab=true;     
+    }
+  }
+  ingresosRetenciones(completed:boolean){
+    if(this.certingresos){
+      this.certingresos=false;
+    }else{
+      this.certingresos=true;     
+    }
+  }
+  soliSubsidio(completed:boolean){
+    if(this.formsolicitud){
+      this.formsolicitud=false;
+    }else{
+      this.formsolicitud=true;     
+    }
+  }
+  soliBecaAlimentacion(completed:boolean){
+    if(this.carta){
+      this.carta=false;
+    }else{
+      this.carta=true;     
+    }
+  }
+  declaracionVecindad(completed:boolean){
+    if(this.vecindad){
+      this.vecindad=false;
+    }else{
+      this.vecindad=true;     
+    }
+  }
+  constaciaWeb(completed:boolean){
+    if(this.constancia){
+      this.constancia=false;
+    }else{
+      this.constancia=true;     
+    }
+  }
+  promAcumulado(completed:boolean){
+    if(this.promacumulado){
+      this.promacumulado=false;
+    }else{
+      this.promacumulado=true;     
+    }
+  }
+  diagnosticoMedico(completed:boolean){
+    if(this.diagnosticomed){
+      this.diagnosticomed=false;
+    }else{
+      this.diagnosticomed=true;     
+    }
+  }
+  documentoD10(completed:boolean){
+    if(this.formatod10){
+      this.formatod10=false;
+    }else{
+      this.formatod10=true;     
+    }
+  }
+  documentoPadre(completed:boolean){
+    if(this.documentopadre){
+      this.documentopadre=false;
+    }else{
+      this.documentopadre=true;     
+    }
+  }
+  documentoMadre(completed:boolean){
+    if(  this.documentomadre){
+      this.documentomadre=false;
+    }else{
+      this.documentomadre=true;     
+    }
+  }
+  carnetEstudiantil(completed:boolean){
+    if(this.carnet){
+      this.carnet=false;
+    }else{
+      this.carnet=true;     
+    }
+  }
+
+
+
+  checkValue(event: any){
+    console.log(event);
+ }
 }
