da2452c81cbf9cdc0f8c1dfa665e2111e00ef22e
AlejoOspina01 <alejandro.ospina@correounivalle.edu.co>
2021-03-28 12:59:28 -0500

    Funcionalidad encuesta

---
+++ src/app/services/encuesta.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EncuestaService {
+
+	url="http://localhost/sigbeweb";
+
+  constructor(private http: HttpClient) { }
+
+    encuestaUser(iduser:any,idperiodo:any){
+		return this.http.get(`${this.url}/app/componentes/encuesta/listEncuestaUserPeriodo.php?idperiodo=${idperiodo}&iduser=${iduser}`);
+	}
+
+	agregarEncuesta(encuesta:any){
+		return this.http.post(`${this.url}/app/componentes/encuesta/crearEncuesta.php`,{data: encuesta})
+		.pipe(map((res)=>{
+			return encuesta;
+		}));
+	}
+}
